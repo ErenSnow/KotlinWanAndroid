9a18ee9d6d437da03840da2ef22173032c61eccd
ErenSnow <erensnowli@gmail.com>
2020-07-15 00:28:45 +0800

    布局改为ConstraintLayout图形化模式 优化主页BottomNavigation

--- app/src/main/java/com/snow/snowwanandroid/MainActivity.kt
+++ app/src/main/java/com/snow/snowwanandroid/MainActivity.kt
@@ -2,25 +2,43 @@ package com.snow.snowwanandroid
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
-
+/**
+ * 主界面
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val navHostFragment = fragmentMain as NavHostFragment
-//        val navController = navHostFragment.navController
-//        bnvMain.setupWithNavController(navController)
+        // 初始化底部导航栏
+        initBottomNavigation()
+    }
 
-        val navController = Navigation.findNavController(this, R.id.fragmentMain)
+    /**
+     * 初始化底部导航栏
+     */
+    private fun initBottomNavigation() {
+        // NavHostFragment对象
+        val navHostFragment = fragmentMain as NavHostFragment
+        // Nav控制器
+        val navController = navHostFragment.navController
+        // 应用栏对象
         val configuration: AppBarConfiguration = AppBarConfiguration.Builder(bnvMain.menu).build()
-        NavigationUI.setupActionBarWithNavController(this, navController, configuration)
-        NavigationUI.setupWithNavController(bnvMain, navController)
+        // 通过Nav底部导航栏与Nav控制器相互关联
+        bnvMain.setupWithNavController(navController)
+        // 通过Nav设置操作栏
+        setupActionBarWithNavController(navController, configuration)
+        // 控制更改监听
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            // 弹出返回栈，防止出现点击返回键回退到默认Fragment
+            controller.popBackStack()
+        }
     }
 }
